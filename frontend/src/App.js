b6c0dbfdd86f2adf617dd9cfaaf856226673efb2
Deren <derenlin123456@gmail.com>
2019-09-28 20:44:46 -0400

    upload form design finished

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,19 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useEffect } from "react";
+import UploadForm from './components/form.js'
+
+const Header = () => (
+  <div style={{height: 46, borderBottom: '1px solid #e3e3e3', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+    <input style={{padding: 4}} type='text' disabled placeholder='TextBox'/>
+  </div>
+)
 
 const App = () => {
+  
   return (
-    <div className="App">
+    <div>
+      <Header/>
     </div>
-  );
-};
+  )
+}
 
 export default App;
