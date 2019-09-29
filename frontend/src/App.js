fa9aa9d08a0721f4b5cabd912b2ae917ce3494d4
Deren <derenlin123456@gmail.com>
2019-09-29 00:02:51 -0400

    form is done!!!!!!

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,19 @@
 import React, { useState, useEffect } from "react";
+import firebase from 'firebase'
 import UploadForm from './components/form.js'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAnHDEuW-Wv7eJ0coqtyD_UO-HtCXYir-0",
+  authDomain: "textbox-b83ce.firebaseapp.com",
+  databaseURL: "https://textbox-b83ce.firebaseio.com",
+  projectId: "textbox-b83ce",
+  storageBucket: "textbox-b83ce.appspot.com",
+  messagingSenderId: "1025744947126",
+  appId: "1:1025744947126:web:c44aab1337fd699cbb2317",
+  measurementId: "G-3MMFE50B69"
+};
+
+firebase.initializeApp(firebaseConfig)
 
 const Header = () => (
   <div style={{height: 46, borderBottom: '1px solid #e3e3e3', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
@@ -8,6 +22,11 @@ const Header = () => (
 )
 
 const App = () => {
+
+  const uploadMedia = (file) => {
+    const imageRef = firebase.storage().ref().child(`TextBoxImages/+19172505500-${file.name}`)
+    return imageRef.put(file).then(snapshot => snapshot.ref.getDownloadURL().then(url => url))
+  }
   
   return (
     <div>
--- frontend/src/components/form.js
+++ frontend/src/components/form.js
@@ -1,10 +1,14 @@
 import React, { useState } from 'react'
+import ReactLoading from 'react-loading'
 
-const UploadForm = () => {
+const UploadForm = ({uploadMedia}) => {
     const [type, setType] = useState('text')
     const [text, setText] = useState('')
     const [media, setMedia] = useState(null)
-    const [name, setName] = useState('')
+    const [title, setTitle] = useState('')
+    const [loading, setLoading] = useState(false)
+    const [response, setResponse] = useState('')
+    const [error, setError] = useState({titleError: "", bodyError: "", mediaError: ""})
 
     const buttonStyle = {
         width: 200,
@@ -16,20 +20,83 @@ const UploadForm = () => {
     }
 
     const upload = () => {
-        
-    }
-
-    const cancel = () => {
-        
+        if(type == 'media') {
+            if(media == null) {
+                setError(prevState => {
+                    return {...prevState, mediaError: 'Invalid/No media file selected'}
+                })
+            }
+            if(title == "") {
+                setError(prevState => {
+                    return {...prevState, titleError: 'Invalid/No file name'}
+                })
+            }
+            if(media && title != "") {
+                console.log('no errors')
+                uploadMedia(media).then(url => {
+                    setLoading(true)
+                    fetch('https://b6300b89.ngrok.io/', {
+                        method: 'PUT',
+                        headers: {'Content-Type': 'application/json'},
+                        body: JSON.stringify({
+                            phone: '+19172505500',
+                            type: 'media',
+                            data: url,
+                            name: title
+                        })
+                    }).then(res => {
+                        setLoading(false)
+                        res.status == 200 ? setResponse('Success!') : setResponse('Failed to upload.')
+                        setTimeout(() => window.location.reload(true), 2000)
+                    })
+                })
+            }
+        } else if (type == 'text') {
+            if(text == '') {
+                setError(prevState => {
+                    return {...prevState, bodyError: 'Invalid/No text'}
+                })
+            }
+            if(title == '') {
+                setError(prevState => {
+                    return {...prevState, titleError: 'Invalid/No file name'}
+                })
+            }
+            if(title != '' && text != '') {
+                setLoading(true)
+                fetch('https://b6300b89.ngrok.io/', {
+                    method: 'PUT',
+                    headers: {'Content-Type': 'application/json'},
+                    body: JSON.stringify({
+                        phone: '+19172505500',
+                        type: 'text',
+                        data: text,
+                        name: title
+                    })
+                    }).then(res => {
+                        setLoading(false)
+                        res.status == 200 ? setResponse('Success!') : setResponse('Failed to upload.')
+                        setTimeout(() => window.location.reload(true), 2000)
+                    })
+            }
+        }
     }
 
     return (
         <div style={{border: '1px solid #e3e3e3', height: 400, width: 400, display: 'flex', flexDirection: 'column', alignItems: 'center', position: 'relative'}}>
-            <input style={{width: '90%', border: 'none', height: 30, borderBottom: '2px solid #e1e1e1', padding: 4, fontSize: '1.2em', outline: 'none'}}
-                placeholder='File Name'
-                value={name}
-                onChange={(e) => setName(e.target.value)}
-            />
+            <div>
+                <input style={{width: '', border: 'none', height: 30, borderBottom: '2px solid #e1e1e1', padding: 4, fontSize: '1.2em', outline: 'none'}}
+                    placeholder='File Name'
+                    value={title}
+                    onChange={(e) => {
+                        setTitle(e.target.value)
+                        setError(prevState => {
+                            return {...prevState, titleError: ""}
+                        })
+                    }}
+                />
+                <p style={{color: '#f7443e', margin: 6, height: 8}}>{error.titleError}</p>
+            </div>
             <div style={{display: 'flex', margin: 0, marginTop: 30}}>
                 <button style={{...buttonStyle, backgroundColor: type == 'text' ? '#5cc0ff' : 'white', color: type == 'text' ? 'white' : 'black'}}
                     onClick={() => setType('text')}
@@ -45,13 +112,19 @@ const UploadForm = () => {
             {type == 'text' ? 
                 <div style={{marginTop: 40}}>
                     <textarea
-                        style={{width: 300, height: 150, padding: 8}}
-                        onChange={(e) => setText(e.target.value)}
+                        style={{width: 300, height: 150, padding: 8, resize: 'none'}}
+                        onChange={(e) => {
+                            setText(e.target.value)
+                            setError(prevState => {
+                                return {...prevState, bodyError: ""}
+                            })
+                        }}
                         value={text}
                     />
-                </div> 
-                : 
-                <div style={{position: 'relative', overflow: 'hidden', marginTop: 80}}>
+                    <p style={{color: '#f7443e', margin: 6}}>{error.bodyError}</p>
+                </div>
+                :
+                <div style={{position: 'relative', overflow: 'hidden', marginTop: 80, display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
                     <input 
                         style={{
                             position: 'absolute',
@@ -62,24 +135,41 @@ const UploadForm = () => {
                             height: 40
                         }}
                         type='file'
+                        onChange={(e) => {
+                            setMedia(e.target.files[0])
+                            setError(prevState => {
+                                return {...prevState, mediaError: ""}
+                            })
+                        }}
                     />
                     <button style={{border: 'none', height: 40, width: 150, backgroundColor: '#5cc0ff', borderRadius: 25, color: 'white', fontSize: '1.1em', cursor: 'pointer'}}>
                         Upload
                     </button>
+                    {media && <p style={{color: '#5cc0ff', margin: 6}}>{media.name}</p>}
+                    <p style={{color: '#f7443e', margin: 6}}>{error.mediaError}</p>
                 </div>
             }
-            <div style={{position: 'absolute', bottom: 10, right: 10}}>
-                <button style={{margin: 4, width: 75, height: 30, borderStyle: 'none', backgroundColor: 'white', cursor: 'pointer', color: 'grey', fontSize: 16, padding: 4}}
-                    onClick={() => cancel()}
-                >
-                    Cancel
-                </button>
-                <button style={{margin: 4, width: 75, height: 30, borderStyle: 'none', borderRadius: 25, backgroundColor: '#5cc0ff', border: '2px solid #5cc0ff', cursor: 'pointer', color: 'white', fontSize: 16, padding: 4}}
-                    onClick={() => upload()}
-                >
-                    Save
-                </button>
-            </div>
+            {
+                !loading && response == '' ?
+                <div style={{position: 'absolute', bottom: 10, right: 10}}>
+                    <a style={{margin: 4, width: 75, height: 30, borderStyle: 'none', backgroundColor: 'white', cursor: 'pointer', color: 'grey', fontSize: 16, padding: 4, textDecoration: 'none'}}
+                        href='/'
+                    >
+                        Cancel
+                    </a>
+                    <button style={{margin: 4, width: 75, height: 30, borderStyle: 'none', borderRadius: 25, backgroundColor: '#5cc0ff', border: '2px solid #5cc0ff', cursor: 'pointer', color: 'white', fontSize: 16, padding: 4}}
+                        onClick={() => upload()}
+                    >
+                        Save
+                    </button>
+                </div>
+                :
+                loading && response == '' ?
+                <div style={{position: 'absolute', bottom: 20, right: 20}}>
+                    <ReactLoading  height={30} width={50} type={'bubbles'} color={'#5cc0ff'}/>
+                </div>
+                : <div style={{fontSize: '1.1em', color: '#5cc0ff'}}>{response}</div>
+            }
         </div>
     )
 }
